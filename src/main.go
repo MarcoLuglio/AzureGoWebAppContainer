ee7b032b89a486c3429f320ea8951193f7f202fd
Marco Luglio <marcodejulho@gmail.com>
2020-10-08 01:49:19 -0300

    Reordered function declarations

--- src/main.go
+++ src/main.go
@@ -14,11 +14,36 @@ import (
 // import
 // "github.com/Azure/go-autorest/autorest/to" // not sure what this does yet
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You just browsed page (if blank you're at the root): %s", r.URL.Path[1:])
-}
-
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
+}
+
+func handler(responseWriter http.ResponseWriter, request *http.Request) {
+
+	var err error
+
+	switch request.Method {
+	case "GET":
+		err = handleGet(responseWriter, request)
+		/*case "POST":
+			err = handleVideoAPIPost(writer, request, video)
+		case "PUT":
+			err = handleVideoAPIPut(writer, request, video)
+		case "DELETE":
+			err = handleVideoAPIDelete(writer, request, video)*/
+	}
+
+	if err != nil {
+		//util.CheckError(err)
+		//return
+	}
+
+}
+
+func handleGet(responseWriter http.ResponseWriter, request *http.Request) (err error) {
+	responseWriter.WriteHeader(500)
+	responseWriter.Header().Set("Content-Type", "application/json")
+	fmt.Print(responseWriter, `{"hello":"world"}`)
+	return
 }
