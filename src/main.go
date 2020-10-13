dcd63a56e82877aa460d65a395167d65438b4ba3
Marco Luglio <marcodejulho@gmail.com>
2020-10-13 01:57:36 -0300

    Implementing timeouts in the requests

--- src/main.go
+++ src/main.go
@@ -8,19 +8,54 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // import
 // "github.com/Azure/go-autorest/autorest/to" // not sure what this does yet
 
 func main() {
+
 	fmt.Println("Starting go server on port 80")
-	http.HandleFunc("/", handler)
-	//http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
-	http.ListenAndServe(":80", nil)
+
+	// check https://medium.com/@simonfrey/go-as-in-golang-standard-net-http-config-will-break-your-production-environment-1360871cb72b
+	handler := timeoutHandler{}
+	timeoutServer := &http.Server{
+		ReadHeaderTimeout: 20 * time.Second,
+		ReadTimeout:       1 * time.Minute,
+		WriteTimeout:      2 * time.Minute,
+		Handler:           handler,
+		Addr:              ":80",
+	}
+	timeoutServer.ListenAndServe()
+
 }
 
-func handler(responseWriter http.ResponseWriter, request *http.Request) {
+// #region timeoutHandler struct
+
+type timeoutHandler struct{}
+
+func (handler timeoutHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+
+	defer request.Body.Close()
+
+	switch request.RequestURI {
+	case "/":
+		// OK
+	default:
+		// TODO 404
+		return
+	}
+
+	/*
+		// this is how to do a varible timeout to read the request, but it will also eat part of the write timeout
+		timer := time.AfterFunc(5*time.Second, func() {
+			request.Body.Close()
+		})
+
+		// if more time is needed to write the response, reset the timer here, os in another method
+		timer.Reset(1 * time.Second)
+	*/
 
 	var err error
 
@@ -43,6 +78,8 @@ func handler(responseWriter http.ResponseWriter, request *http.Request) {
 	}
 
 }
+
+// #endregion
 
 func handleOptions(responseWriter http.ResponseWriter, request *http.Request) (err error) {
 	responseWriter.Header().Set("access-control-allow-origin", "*") // posso usar *, mas não se requerir autenticação com bearer:
